e809883291e9c834eedc3760465ac196f596b276
Luciano <lucianomartin2015nqn@gmail.com>
2022-02-21 18:56:35 -0300

    Agrega mensaje de fecha

--- PrimerGitJava.java
+++ PrimerGitJava.java
@@ -5,6 +5,7 @@ public class PrimerGitJava
 	public static void main(String[] args)
 	{
 		System.out.println("Hola GIT");
+		System.out.println("La fecha actual es:");
 		System.out.println(LocalDate.now());
 	}
 }
