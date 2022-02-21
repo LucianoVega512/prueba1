0f8deee3ef3a986f036666762f4c1b3472191a32
Luciano <lucianomartin2015nqn@gmail.com>
2022-02-21 18:51:30 -0300

    Se agrega la hora actual

--- PrimerGitJava.java
+++ PrimerGitJava.java
@@ -1,7 +1,10 @@
+import java.time.LocalDate;
+
 public class PrimerGitJava
 {
 	public static void main(String[] args)
 	{
-		
+		System.out.println("Hola GIT");
+		System.out.println(LocalDate.now());
 	}
 }
